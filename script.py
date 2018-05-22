34d4406dbb4d5264812529bdbd88fa94fd91f6f1
feliperafael <feliperafael_95@yahoo.com.br>
2018-05-22 06:20:21 -0300

    plot function

--- script.py
+++ script.py
@@ -8,6 +8,12 @@ Created on Fri May 18 16:16:53 2018
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
+
+owls_init   = [150,150,100,10]
+mouses_init = [200,300,200,20]
+
+times = 100
+
 #@return new amount of owls 
 def owl(owls_now, mouses_now):
     return (0.7*owls_now + 0.002*mouses_now*owls_now)
@@ -15,19 +21,7 @@ def owl(owls_now, mouses_now):
 def mouse(mouses_now, owls_now):
     return (1.2*mouses_now - 0.001*mouses_now*owls_now)
 
-owls_init   = [150,150,100,10]
-mouses_init = [200,300,200,20]
-
-times = 100
-
-for scenario in range(len(owls_init)):
-    owls   = [owls_init[scenario]]
-    mouses = [mouses_init[scenario]]
-    
-    for time in range(times):
-        owls.append( owl(owls[time], mouses[time]) )
-        mouses.append( mouse(mouses[time], owls[time]) )
-    
+def plot(owls,mouses):    
     plt.figure(1)
     plt.subplot(121)
     plt.plot(owls,mouses,'r')
@@ -47,4 +41,14 @@ for scenario in range(len(owls_init)):
     plt.subplots_adjust(top=0.9, bottom=0.1, left=0.15, right=0.95, hspace=0.30,
                     wspace=0.35)
     plt.show()
+
+for scenario in range(len(owls_init)):
+    owls   = [owls_init[scenario]]
+    mouses = [mouses_init[scenario]]
     
+    for time in range(times):
+        owls.append( owl(owls[time], mouses[time]) )
+        mouses.append( mouse(mouses[time], owls[time]) )
+
+    plot(owls,mouses)
+
